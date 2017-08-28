0a72de718e7869e8bdd37eadf5784f2f7dc90652
laiyongshan <850422483@qq.com>
2017-08-28 18:01:36 +0800

    封装好的okhttp工具Demo

---
+++ app/src/main/java/com/yeohe/myokhttpapplication/sample_okhttp/URLs.java
@@ -0,0 +1,13 @@
+package com.yeohe.myokhttpapplication.sample_okhttp;
+
+/**
+ * Created by Administrator on 2017/8/17.
+ */
+
+public class URLs {
+
+    public final static String HOST="http://qr.yeohe.com/testfeigoucar/index.php/API2";//测试接口
+
+    public final static String GET_REGISTER_CITY=HOST+"/ClientCGS/getRegisterCity.html";//获取注册省份的城市信息（包含省份注册提醒文案）
+
+}
